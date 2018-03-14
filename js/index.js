350b2ee5c3e78d186e529d016937caea3efab68d
shadowczp <czp5153409@qq.com>
2018-03-14 22:27:42 +0800

    1

---
+++ js/index.js
@@ -0,0 +1,32 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!',
+        seen: true,
+        todos: [
+            {
+                text: "第一条消息"
+            },
+            {
+                text: "第二条消息"
+            },
+            {
+                text: "第三条消息"
+            }],
+        groceryList: [
+            {id: 0, text: '蔬菜'},
+            {id: 1, text: '奶酪'},
+            {id: 2, text: '随便其它什么人吃的东西'}
+        ]
+    },
+    methods: {
+        reverseMessage: function () {
+            this.message = this.message.split('').reverse().join('')
+        }
+    },
+    computed: {
+        fuck: function () {
+            return Date.now()
+        }
+    }
+})
